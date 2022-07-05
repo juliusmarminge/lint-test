76a1980c989d31d2f727a7cc792e5afc8b0c8803
Julius Marminge <51714798+juliusmarminge@users.noreply.github.com>
2022-07-06 01:56:50 +0200

    test other redirect

--- middleware.ts
+++ middleware.ts
@@ -2,7 +2,8 @@ import type { NextRequest } from "next/server";
 import { NextResponse } from "next/server";
 
 export function middleware(request: NextRequest) {
-  const url = request.nextUrl.clone();
+  return NextResponse.redirect(new URL("/", request.url));
+  /*onst url = request.nextUrl.clone();
   url.pathname = "/dest";
-  return NextResponse.rewrite(url);
+  return NextResponse.rewrite(url);*/
 }
