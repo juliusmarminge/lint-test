8e17721daa5c98d98d61f5bb15f89fd8e47a9667
Julius Marminge <51714798+juliusmarminge@users.noreply.github.com>
2022-07-06 01:49:09 +0200

    add middleware redirect

---
+++ pages/dest.tsx
@@ -0,0 +1,10 @@
+import type { NextPage } from "next";
+
+const RedirectPage: NextPage = () => {
+  return (
+    <div>
+      <h1>All goes here!</h1>
+    </div>
+  );
+};
+export default RedirectPage;
